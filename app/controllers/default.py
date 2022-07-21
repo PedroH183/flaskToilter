10c8b5bb9a93a8a16cb7a556fbf2b7ea22e424a6
pedro <pedro_freires18@outlook.com>
2022-07-21 17:42:37 -0300

    adicionando o formulario no repositorio

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -1,14 +1,24 @@
 from flask import render_template
 from app import app
+from app.models.forms import LoginForm
 
-@app.route('/home/<user>')
-@app.route('/home', defaults = {'user':None})
-def home(user):
-    return render_template('home.html', user = user) # o flask vai buscar por padrão dentro dentro da pasta template
 
-@app.route('/home/base/')
-def perfil():
-    return render_template('base.html')
+@app.route('/home/')
+def tentando():
+    return render_template('home.html')
+
+
+@app.route('/login/', methods = ["Get","Post"])
+def home():
+    form = LoginForm()
+    
+    if form.validate_on_submit():
+        print(form.username.data)
+        print(form.password.data)
+    else:
+        print(form.errors)
+
+    return render_template('login.html', form = form) # o flask vai buscar por padrão dentro dentro da pasta template
 
 
 """
--- config.py
+++ config.py
@@ -2,3 +2,5 @@ DEBUG = True # ativa o relod automatico
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///storage.db'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+SECRET_KEY = 'TaSeguro'
